e478a831a82d2f048de025828ed4ca8efe436a93
Anton <useanton@gmail.com>
2016-08-16 15:27:29 +0300

    this is comment

---
+++ git_test/MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var name = "Hui"
+
+if name == "Hui"{
+
+    print("our name is censored")
+} else {
+    print("Your name is " + name)
+}
+
